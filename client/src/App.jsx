1077e4ec2fa3caf7bf6cf4d1580150dc0efb1bc7
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-22 17:49:02 +0530

    created pages and routes are setted up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import Profile from "./assets/pages/Profile";
+import SignIn from "./assets/pages/SignIn";
+import SignUp from "./assets/pages/SignUp";
 
 const App = () => {
-  return <>Hello World!</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
